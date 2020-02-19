86cba31f0c6effaf4b0e8559d2feb6f8d6afc663
James Morris <blondfro@gmail.com>
2020-02-18 18:57:28 -0600

    created a set of basic routes for crud.

--- index.js
+++ index.js
@@ -4,6 +4,26 @@ import data from "./data/data";
 const app = express();
 const PORT = 3000;
 
+// get some data
+app.get('/', (req, res) => {
+    res.send("a get request with / route on port" + PORT)
+});
+
+// add some new data
+app.post('/newItem', (req, res) => {
+    res.send("a post request with /newItem route on port" + PORT)
+});
+
+// update some existing data
+app.put('/updateItem', (req, res) => {
+    res.send("a put request with /updateItem route on port" + PORT)
+});
+
+// delete some data
+app.delete('/item', (req, res) => {
+    res.send("a delete request with /item route on port" + PORT)
+});
+
 app.listen(PORT, () => {
     console.log("Your server is running on port" + PORT);
     console.log(data);
